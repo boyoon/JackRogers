480eff7cbc0767ad20c65a0cdd0442de7125e3eb
boyoon <bominyoon42291@gmail.com>
2018-11-29 09:46:19 -0500

    Mobile Custom Jack Page

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -35,6 +35,12 @@
 // =require vendor/rivets.min.js
 
 
+/*!
+ * slick.js
+ */
+// =require vendor/slick.js
+
+
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
 //
